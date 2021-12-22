156a9d7b133bb1491ad1b18d93ee2143a91e537c
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2021-12-23 01:02:18 +0200

    init commit

---
+++ js/drinks.js
@@ -0,0 +1,14 @@
+function first(){
+  document.getElementById("slideimage").src="/img/two.jpg";
+}
+setInterval(first,2000);
+
+function secound(){
+  document.getElementById("slideimage").src="/img/three.jpg";
+}
+setInterval(secound,4000);
+
+function third(){
+  document.getElementById("slideimage").src="/img/one.jpg";
+}
+setInterval(third,6000);
